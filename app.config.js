5193f17e2db9ff83b536aa1b5ab8c279c68507ce
Amire Steve <dharyore@gmail.com>
2022-07-20 04:34:23 +0100

    Base Setup for POS

---
+++ app.config.js
@@ -0,0 +1,55 @@
+let IS_DEV = {
+  name: "SalesPOS",
+  bundleIdentifier: "com.prerit.salespos",
+  package: "com.mypackage.salespos",
+  icon: "./assets/icon.png",
+};
+
+if (process.env.APP_ENV === "production") {
+  IS_DEV.name = "Flickwheel";
+  IS_DEV.bundleIdentifier = "com.prerit.salespos";
+  IS_DEV.package = "com.mypackage.salespos";
+  IS_DEV.icon = "./assets/icon.png";
+  //IS_DEV.googleServicesFile = "./google-services.json";
+  //IS_DEV.oneSignal = "production";
+} else if (process.env.APP_ENV === "preview") {
+  IS_DEV.name = "Flickwheel Beta";
+  IS_DEV.bundleIdentifier = "com.salespos.preview";
+  IS_DEV.package = "com.mypackage.prevsalespos";
+  IS_DEV.icon = "./assets/icon_preview.png";
+  //IS_DEV.googleServicesFile = "./google-services-preview.json";
+  //IS_DEV.oneSignal = "production";
+} else {
+  IS_DEV.name = "Flickwheel Dev";
+  IS_DEV.bundleIdentifier = "com.salespos.development";
+  IS_DEV.package = "com.mypackage.devsalespos";
+  IS_DEV.icon = "./assets/icon_dev.png";
+  //IS_DEV.googleServicesFile = "./google-services-dev.json";
+  //IS_DEV.oneSignal = "development";
+}
+
+export default ({ config }) => {
+  //console.log({ config });
+  return {
+    ...config,
+    version: "1.0.0",
+    name: IS_DEV.name,
+    icon: IS_DEV.icon,
+    android: {
+      ...config.android,
+      package: IS_DEV.package,
+      versionCode: 1,
+      googleServicesFile: IS_DEV.googleServicesFile,
+      adaptiveIcon: {
+        foregroundImage: IS_DEV.icon,
+      },
+    },
+    ios: {
+      ...config.ios,
+      icon: IS_DEV.icon,
+      bundleIdentifier: IS_DEV.bundleIdentifier,
+      buildNumber: "1",
+    },
+    plugins: [...config.plugins],
+  };
+};
